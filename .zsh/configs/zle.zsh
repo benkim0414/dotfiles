79bbc08406fe71ba0bab11e67d41d3394b65307e
Ben Kim <gunwoo@gunwoo.org>
2022-05-17 11:37:29 +1000

    Add keybindings

--- .zsh/configs/zle.zsh
+++ .zsh/configs/zle.zsh
@@ -2,3 +2,15 @@
 # when the shell starts up then it will be `viins'. 
 export EDITOR="vim"
 
+bindkey "^a" beginning-of-line
+bindkey "^e" end-of-line
+bindkey "^f" forward-char
+bindkey "^b" backward-char
+bindkey "^k" kill-line
+bindkey "^d" delete-char
+bindkey "^p" history-search-backward
+bindkey "^n" history-search-forward
+bindkey "^y" accept-and-hold
+bindkey "^w" backward-kill-word
+bindkey "^u" backward-kill-line
+
