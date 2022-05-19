4cbae2d709b3e262de7758372b4458c78cdfd774
Gunwoo Ben Kim <benkim0414@gmail.com>
2022-05-19 14:23:32 +1000

    Define mappings to move between treesitter text objects

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,6 +132,26 @@ require('nvim-treesitter.configs').setup {
         ['al'] = '@loop.outer',
       },
     },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
   },
 }
 
