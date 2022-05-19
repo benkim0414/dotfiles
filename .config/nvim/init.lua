a4018353f03d093af1ecb26354c662d2dde9810f
Gunwoo Ben Kim <benkim0414@gmail.com>
2022-05-19 15:01:34 +1000

    Install pyright language server

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -49,7 +49,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = {'tsserver'}
+local servers = {'tsserver', 'pyright'}
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
