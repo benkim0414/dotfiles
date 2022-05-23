413fe0c7cff37b9dcc0bd056f2c59d2ae14ac7d3
Gunwoo Ben Kim <benkim0414@gmail.com>
2022-05-23 12:24:36 +1000

    Add missing hrsh7th/cmp-nvim-lsp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -39,7 +39,7 @@ return require('packer').startup(function()
 
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
-
+  use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/vim-vsnip'
 
