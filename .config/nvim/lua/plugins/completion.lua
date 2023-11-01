9f52c22ead597c776de07b2e02344e7b5c392491
Gunwoo Ben Kim <benkim0414@gmail.com>
2023-11-01 16:53:38 +1100

    refactor(nvim): extract plugins for completion

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "zbirenbaum/copilot-cmp",
+    },
+    config = function()
+      local api = vim.api
+      local fn = vim.fn
+      local utils = require("utils")
+      local nnoremap = utils.nnoremap
+
+      nnoremap("<Space>e", "<Cmd>lua vim.diagnostic.open_float()<CR>")
+      nnoremap("[d", "<Cmd>lua vim.diagnostic.goto_prev()<CR>")
+      nnoremap("]d", "<Cmd>lua vim.diagnostic.goto_next()<CR>")
+      nnoremap("<Space>q", "<Cmd>lua vim.diagnostic.setloclist()<CR>")
+
+      local function has_words_before()
+        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+          return false
+        end
+        local line, col = table.unpack(api.nvim_win_get_cursor(0))
+        return col ~= 0 and api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+      end
+
+      local function feedkey(key, mode)
+        api.nvim_feedkeys(api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({select = true}),
+          ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
+            elseif fn["vsnip#available"](1) == 1 then
+              feedkey("<plug>(vsnip-expand-or-jump)", "")
+            else
+              fallback()
+            end
+          end),
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif fn["vsnip#jumpable"](-1) == 1 then
+              feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+          end, {"i", "s"}),
+        }),
+        sources = cmp.config.sources({
+          {name = "copilot"},
+          {name = "nvim_lsp"},
+          {name = "nvim_lua"},
+          {name = "vsnip"},
+          {name = "buffer"},
+          {name = "path"},
+        }),
+      })
+    end,
+  },
+}
