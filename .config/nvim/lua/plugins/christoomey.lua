88788460c6c405f5b41f0f6381f9a868f1250181
Gunwoo Ben Kim <benkim0414@gmail.com>
2023-11-08 11:06:10 +1100

    fix(nvim): extract christoomey plugins

---
+++ .config/nvim/lua/plugins/christoomey.lua
@@ -0,0 +1,5 @@
+return {
+  "christoomey/vim-system-copy",
+  "christoomey/vim-tmux-navigator",
+  "christoomey/vim-tmux-runner",
+}
--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,6 +1,4 @@
 return {
-  "christoomey/vim-system-copy",
-  "christoomey/vim-tmux-navigator",
   "pbrisbin/vim-mkdir",
   "vim-scripts/ReplaceWithRegister",
 }
