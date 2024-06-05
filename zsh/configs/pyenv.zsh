46a9ca78f8f9aeec4183e3433ce27d630622787a
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-06-05 11:24:18 +1000

    fix(zsh): load pyenv async

--- zsh/configs/pyenv.zsh
+++ zsh/configs/pyenv.zsh
@@ -1,12 +1,9 @@
-function initialize_pyenv() {
-  eval "$(pyenv init -)"
-}
-
 function initialize_pyenv_virtualenv() {
+  eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
 }
 
 async_start_worker pyenv_worker -n
 async_register_callback pyenv_worker initialize_pyenv_virtualenv
-async_job pyenv_worker initialize_pyenv
+async_job pyenv_worker sleep 0.1
 
