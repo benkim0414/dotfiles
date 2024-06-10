597d337c3d07f1b162496e47b8152086e1be52d5
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-06-10 11:58:08 +1000

    feat(nvim): install octo.nvim

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -22,4 +22,15 @@ return {
   {
     "whiteinge/diffconflicts",
   },
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function ()
+      require("octo").setup()
+    end
+  },
 }
