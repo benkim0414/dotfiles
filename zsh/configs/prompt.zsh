cc1f933f24433adeaf52c5e2205932682c6bbcec
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-06-14 14:14:00 +1000

    feat(starship): configure starship prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,9 +1,1 @@
-# If set, parameter expansion, command substitution and arithmetic expansion
-# are performed in prompts. Substitutions within prompts do not affect the
-# command status.
-setopt PROMPT_SUBST
-. ~/.zsh/prompt/git-prompt.sh
-GIT_PS1_SHOWDIRTYSTATE=true
-GIT_PS1_SHOWCOLORHINTS=true
-PROMPT="%~\$(__git_ps1 '(%s)')%# "
-
+eval "$(starship init zsh)"
