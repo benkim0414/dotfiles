583941c1d336e0368deaa8e20b10910365274643
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-07-07 09:22:43 +0100

    fix(nvim): add missing LSP keymaps

--- tag-nvim/config/nvim/lua/plugins/lspconfig.lua
+++ tag-nvim/config/nvim/lua/plugins/lspconfig.lua
@@ -12,6 +12,31 @@ return {
       local default_capabilities = vim.lsp.protocol.make_client_capabilities()
       local capabilities = require("cmp_nvim_lsp").default_capabilities(default_capabilities)
 
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+          local opts = {buffer = ev.buf}
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<Space>wa", vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set("n", "<Space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set("n", "<Space>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set("n", "<Space>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<Space>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set({"n", "v"}, "<Space>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<Space>f", function()
+            vim.lsp.buf.format {async = true}
+          end, opts)
+        end,
+      })
+
       require("mason").setup()
       require("mason-lspconfig").setup {
         ensure_installed = {"lua_ls", "pyright"},
