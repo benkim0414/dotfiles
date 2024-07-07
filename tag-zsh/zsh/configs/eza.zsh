ff4c10c4a91b697453205eb032b1f92866f521a9
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-07-08 09:49:54 +1000

    feat(eza): add aliases

--- tag-zsh/zsh/configs/eza.zsh
+++ tag-zsh/zsh/configs/eza.zsh
@@ -1,1 +1,11 @@
-alias ls=eza
+export EZA_ICONS_AUTO=true
+# List only directories
+alias ld='eza -lD'
+# List only files
+alias lf='eza -lf --color=always | grep -v /'
+# List only hidden files
+alias lh='eza -dl .* --group-directories-first'
+# List everything with directories first
+alias ls='eza -al --color=always --group-directories-first'
+# List everything sorted by time updated
+alias lt='eza -al --sort=modified'
