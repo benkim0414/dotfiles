0b27f5dfea2ee264632ce48a963162d57b3beb35
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-09-15 20:41:21 +1000

    fix(zsh): remove long option from ls

--- tag-zsh/zsh/configs/eza.zsh
+++ tag-zsh/zsh/configs/eza.zsh
@@ -6,6 +6,6 @@ alias lf='eza -lf --color=always | grep -v /'
 # List only hidden files
 alias lh='eza -dl .* --group-directories-first'
 # List everything with directories first
-alias ls='eza -al --color=always --group-directories-first'
+alias ls='eza -a --color=always --group-directories-first'
 # List everything sorted by time updated
 alias lt='eza -al --sort=modified'
