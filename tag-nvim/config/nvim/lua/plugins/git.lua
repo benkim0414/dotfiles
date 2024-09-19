a9547a944b67bb95f505c4b82e708f77bd12005b
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-09-20 09:15:24 +1000

    fix(nvim): replace neogit with lazygit

--- tag-nvim/config/nvim/lua/plugins/git.lua
+++ tag-nvim/config/nvim/lua/plugins/git.lua
@@ -1,23 +1,19 @@
 return {
   {
-    "NeogitOrg/neogit",
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "sindrets/diffview.nvim",
-      "ibhagwan/fzf-lua",
     },
-    config = function()
-      local nnoremap = require("utils").nnoremap
-      require("neogit").setup {
-        kind = "split_above",
-      }
-
-      nnoremap("<Leader>gg", "<Cmd>Neogit<CR>")
-      nnoremap("<Leader>gc", "<Cmd>Neogit commit<CR>")
-      nnoremap("<Leader>gl", "<Cmd>Neogit pull<CR>")
-      nnoremap("<Leader>gp", "<Cmd>Neogit push<CR>")
-    end,
+    keys = {
+      { "<Leader>lg", "<Cmd>LazyGit<CR>", desc = "LazyGit" },
+    },
   },
   {
     "lewis6991/gitsigns.nvim",
