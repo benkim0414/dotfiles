e0d5450c7f28cd4bab7feb1f960d74ea9666c9af
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-11-02 20:41:40 +1100

    fix(nvim): replace config with opts in conform config

--- tag-nvim/config/nvim/lua/plugins/conform.lua
+++ tag-nvim/config/nvim/lua/plugins/conform.lua
@@ -1,38 +1,36 @@
 return {
   {
     "stevearc/conform.nvim",
-    optional = true,
-    config = function()
-      require("conform").setup {
-        formatters = {
-          ["markdown-toc"] = {
-            condition = function(_, ctx)
-              for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-                if line:find("<!%-%- toc %-%->") then
-                  return true
-                end
+    opts = {
+        },
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find("<!%-%- toc %-%->") then
+                return true
               end
-            end,
-          },
-          ["markdownlint-cli2"] = {
-            condition = function(_, ctx)
-              local diag = vim.tbl_filter(function(d)
-                return d.source == "markdownlint"
-              end, vim.diagnostic.get(ctx.buf))
-              return #diag > 0
-            end,
-          },
+            end
+          end,
         },
-        formatters_by_ft = {
-          typescript = { "prettier", stop_after_first = true },
-          ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-          ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdownlint-cli2"] = {
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
         },
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_format = "fallback",
-        },
-      }
-    end
+      },
+      formatters_by_ft = {
+        typescript = { "prettier", stop_after_first = true },
+        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+        async = false
+      },
+    }
   }
 }
