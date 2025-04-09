ee0cb86bfd191f2e7cf05a50dd746132f17fedf7
Gunwoo Ben Kim <benkim0414@gmail.com>
2025-04-09 19:31:12 +1000

    feat(nvim): add copilot cmp source

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -65,6 +65,7 @@ return {
           end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
+          { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "buffer" },
           { name = "path" },
